674e9538fa767f1314455dc773a535291c53c107
bpage93 <bradpage93@gmail.com>
2025-09-01 20:51:57 -0600

    added analytics-table

--- src/app/analytics/analytics.ts
+++ src/app/analytics/analytics.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card'; // Import MatCardModule
+import { MatTabsModule } from '@angular/material/tabs';  // Import MatTabsModule
+import { AnalyticsTableComponent } from '../analytics-table/analytics-table';
 
 @Component({
   selector: 'app-analytics',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, MatCardModule, MatTabsModule, AnalyticsTableComponent],
   templateUrl: './analytics.html',
   styleUrls: ['./analytics.css'],
 })
